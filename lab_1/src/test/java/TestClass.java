939130aa4a1573b81238b9017ecdbe4f34b3d6c9
Firemounta1n <xxMAXxx2009@gmail.com>
2016-06-04 19:04:42 +0300

    Update TestClass.java
    Add new tests

--- lab_1/src/test/java/TestClass.java
+++ lab_1/src/test/java/TestClass.java
@@ -11,49 +11,70 @@ public class TestClass {
     @Test
     public void testLeftEdge() throws Exception {
         Paginator paginator = new Paginator();
-        List<Integer> testList = Arrays.asList(1, 2, 0, 10);
-        assertEquals(paginator.getList(1, 10), testList);
+        List<Integer> testList = Arrays.asList(1, 2, 3, 4, 5, 0, 20);
+        assertEquals(paginator.getList(1, 20, 5), testList);
     }
 
     @Test
     public void testRightEdge() throws Exception {
         Paginator paginator = new Paginator();
-        List<Integer> testList = Arrays.asList(1, 0, 9, 10);
-        assertEquals(paginator.getList(10, 10), testList);
+        List<Integer> testList = Arrays.asList(1, 0, 13, 14, 15, 16, 17, 18, 19, 20);
+        assertEquals(paginator.getList(20, 20, 7), testList);
     }
 
     @Test
     public void testSecondPage() throws Exception {
         Paginator paginator = new Paginator();
-        List<Integer> testList = Arrays.asList(1, 2, 3, 0, 10);
-        assertEquals(paginator.getList(2, 10), testList);
+        List<Integer> testList = Arrays.asList(1, 2, 3, 0, 20);
+        assertEquals(paginator.getList(2, 20, 3), testList);
+    }
+
+    @Test
+    public void testThirdPage() throws Exception {
+        Paginator paginator = new Paginator();
+        List<Integer> testList = Arrays.asList(1, 2, 3, 4, 5, 0, 20);
+        assertEquals(paginator.getList(3, 20, 5), testList);
+    }
+
+    @Test
+    public void testFourthPage() throws Exception {
+        Paginator paginator = new Paginator();
+        List<Integer> testList = Arrays.asList(1, 2, 3, 4, 5, 6, 0, 20);
+        assertEquals(paginator.getList(4, 20, 5), testList);
+    }
+
+    @Test
+    public void testCenterPage() throws Exception {
+        Paginator paginator = new Paginator();
+        List<Integer> testList = Arrays.asList(1, 0, 8, 9, 10, 11, 12, 0, 20);
+        assertEquals(paginator.getList(10, 20, 5), testList);
     }
 
     @Test
     public void testPrelastPage() throws Exception {
         Paginator paginator = new Paginator();
-        List<Integer> testList = Arrays.asList(1, 0, 8, 9, 10);
-        assertEquals(paginator.getList(9, 10), testList);
+        List<Integer> testList = Arrays.asList(1, 0, 7, 8, 9, 10);
+        assertEquals(paginator.getList(9, 10, 3), testList);
     }
 
     @Test
     public void testEmptyPages() throws Exception {
         Paginator paginator = new Paginator();
         List<Integer> testList = Arrays.asList();
-        assertEquals(paginator.getList(5, 0), testList);
+        assertEquals(paginator.getList(5, 0, 3), testList);
     }
 
     @Test
     public void testOutOfEdgeRight() throws Exception {
         Paginator paginator = new Paginator();
-        List<Integer> testList = Arrays.asList(1, 0, 9, 10);
-        assertEquals(paginator.getList(11, 10), testList);
+        List<Integer> testList = Arrays.asList(1, 0, 8, 9, 10);
+        assertEquals(paginator.getList(11, 10, 3), testList);
     }
 
     @Test
     public void testOutOfEdgeLeft() throws Exception {
         Paginator paginator = new Paginator();
-        List<Integer> testList = Arrays.asList(1, 2, 0, 10);
-        assertEquals(paginator.getList(0, 10), testList);
+        List<Integer> testList = Arrays.asList(1, 2, 3, 0, 10);
+        assertEquals(paginator.getList(0, 10, 3), testList);
     }
 }
